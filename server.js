6e84659802ace6457796ddbd52cb1527bdb6626c
Shaif Azad <shaif.rahi@northsouth.edu>
2021-06-22 16:52:57 +0600

    latest build pushed with changed port to 8100

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', function (req,res) {
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8200;
+const PORT = process.env.PORT || 8100;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
