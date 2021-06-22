15f562f2e1c06fcf7131c4939065ba4597256869
Shaif Azad <shaif.rahi@northsouth.edu>
2021-06-22 16:47:28 +0600

    assets folder removed from public

--- react-web-app/view/src/routes/Routes.js
+++ react-web-app/view/src/routes/Routes.js
@@ -5,7 +5,9 @@ const Employees = React.lazy(()=> import('../components/employees/Employees'))
 const routes = [
   { path: '/dashboard', exact: true, name: 'Dashboard', component:Dashboard },
   { path: '/dashboard/employees',exact: true, name: 'Employees', component: Employees },
-  { path: '/dashboard/employees/details',exact:true, name: 'Employee Details', component: React.lazy(()=>import('../components/employees/EmployeeDetails'))}
+  { path: '/dashboard/employees/details',exact:true, name: 'Employee Details', component: React.lazy(()=>import('../components/employees/EmployeeDetails'))},
+  { path: '/dashboard/badges/',exact:true, name: 'Badges', component: React.lazy(()=>import('../components/badges/Badges'))},
+  { path: '/dashboard/sitecontrol/',exact:true, name: 'Site Control', component: React.lazy(()=>import('../components/sitecontrol/SiteControl'))}
 ];
 
 export default routes;
