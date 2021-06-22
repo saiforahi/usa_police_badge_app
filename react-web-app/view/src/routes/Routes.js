6514ef212089cd819c130b74012c342ee675d228
Shaif Azad <shaif.rahi@northsouth.edu>
2021-06-22 15:36:24 +0600

    data tables for employees and badges

--- react-web-app/view/src/routes/Routes.js
+++ react-web-app/view/src/routes/Routes.js
@@ -4,7 +4,8 @@ const Dashboard = React.lazy(() => import('../components/dashboard/Dashboard'));
 const Employees = React.lazy(()=> import('../components/employees/Employees'))
 const routes = [
   { path: '/dashboard', exact: true, name: 'Dashboard', component:Dashboard },
-  { path: '/dashboard/employees', name: 'Employees', component: Employees }
+  { path: '/dashboard/employees',exact: true, name: 'Employees', component: Employees },
+  { path: '/dashboard/employees/details',exact:true, name: 'Employee Details', component: React.lazy(()=>import('../components/employees/EmployeeDetails'))}
 ];
 
 export default routes;
