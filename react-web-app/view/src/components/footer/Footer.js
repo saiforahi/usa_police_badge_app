f667c798cdce1bf7726c16592edb75fa4168b3c0
Pranto <prakat1620@gmail.com>
2021-07-09 03:22:08 +0600

    home page design changes

---
+++ react-web-app/view/src/components/footer/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer =()=>{
+    return(
+      
+<footer className="row footer-gap">
+    <div className="col-md-4 text-center">
+<img className="img-fluid footer-images" src={"assets/images/accountability-badge-demo.webp"}/>
+    </div>
+    <div className="col-md-4 text-center">
+        <h4 className="footer-header">#Accountability</h4>
+        <h5 class="footer-minor-details">Global Accountability Corp.</h5>
+        <h5 class="footer-minor-details">P.O. Box 2308</h5>
+        <h5 class="footer-minor-details">Missoula, MT. 59806</h5>
+        <h5 class="footer-minor-details">888-63-BADGE (22343)</h5>
+
+    </div>
+    <div className='col-md-4 text-center'>
+    <img className="img-fluid footer-images" src={"assets/images/safeid-demo.webp"}/>
+    </div>
+    <div className="col-md-12"><p class="copyright-info">&copy;2021 by The Accountability Badge Corporation</p></div>
+</footer>
+
+    )
+}
+
+export default Footer
--- react-web-app/view/src/components/header/header.js
+++ react-web-app/view/src/components/header/header.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import { CIcon } from '@coreui/icons-react'
 import './header.css'
-
+import{Navbar,Nav,Button}from 'react-bootstrap';
 import {Link} from 'react-router-dom'
 
 const Header =()=>{
     return(
-      <header className="header-main dark">
-        <nav>
-          <nav>
-            <Link to="/" name="Home"><img src={'assets/images/web-logo-2.png'}/><span className="logo-imp">The Accountibility Badge</span></Link> 
-            <div className="nav-toggle"></div>
-              <ul className="inline right" id="toggled-menu">
-                <li><Link to= "/login" className="button button-secondary button-m">{localStorage.getItem('access_token')? 'Dashboard':'Login'}</Link></li>
-              </ul>
-            </nav> 
-          </nav>
-      </header>
+<Navbar  expand="lg" className="pt-6">
+  <Navbar.Brand to="/"><img src={'assets/images/logo-home-demo.svg'}></img></Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" className="pb-2"/>
+  <Navbar.Collapse id="basic-navbar-nav ">
+    <Nav className="mr-auto padding-desktop">
+      <Nav.Link to="/">Home</Nav.Link>
+      <Nav.Link to="/">Our Team</Nav.Link>
+      <Nav.Link to="/">Facebook Feed</Nav.Link>
+      <Nav.Link to="/">Contact</Nav.Link>
+     
+    </Nav>
+    <Link to="/login" className="btn btn-outline-success ml-auto sign-btn">{localStorage.getItem('access_token')? 'Dashboard':'Sign In'}</Link>
+  </Navbar.Collapse>
+</Navbar>
     )
    
 }
