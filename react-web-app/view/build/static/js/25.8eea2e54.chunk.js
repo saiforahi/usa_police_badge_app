ee26cf623810764f6f170913d129faf4a7a5c29d
Shaif Azad <shaif.rahi@northsouth.edu>
2021-07-11 18:37:50 +0600

    latest build

---
+++ react-web-app/view/build/static/js/25.8eea2e54.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpusa-nfc-web-app"]=this["webpackJsonpusa-nfc-web-app"]||[]).push([[25],{865:function(n,e,s){"use strict";s.r(e);var t=s(0),a=s.n(t),c=s(736),r=s.n(c),u=s(15);e.default=function(){return a.a.useEffect((function(){r()("Sorry!","This service is under development","warning")}),[]),Object(u.jsx)(u.Fragment,{})}}}]);
+//# sourceMappingURL=25.8eea2e54.chunk.js.map
